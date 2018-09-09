3fb9bc0230826699ae943b4388e81e79c04b9a63
robolux <HyperDrive@66.37-191-171.fiber.lynet.no>
2018-09-09 16:18:03 +0200

    clarified user interface with prompts

--- Projects/Project1/src/main.py
+++ Projects/Project1/src/main.py
@@ -1,6 +1,11 @@
 # Project 1 Main - Computational Physics
 # Hunter Phillips
 
+# This program allows the user to input n, a, b, and c to solve
+# Poisson's equation in a single dimension by turning it into a matrix.
+# using Gaussian Jordan Elimination, Specialized Tridiagonal, and LU Decomposition
+# Benchmarks and error calculations are also performed.
+
 import sys
 import numpy as nmp
 from solver import *
@@ -43,11 +48,11 @@ def part_e(n, a_i, b_i, c_i):
 # benchmarks
 def benchmarking(n, a_i, b_i, c_i):
 
-    a = nmp.full(n+2, a_i, dtype=nmp.float64)
+    a = nmp.full(n+2, a_i, dtype=nmp.float64) # have to get that accuracy
     b = nmp.full(n+2, b_i, dtype=nmp.float64)
     c = nmp.full(n+2, c_i, dtype=nmp.float64)
 
-    start_time = time.time()
+    start_time = time.time() # in the future I would like to add more complex timing analysis
     x, v = general(f_function, a, b, c, n)
     end_time = time.time()
     general_time = end_time - start_time
@@ -71,29 +76,39 @@ def benchmarking(n, a_i, b_i, c_i):
 
 if __name__ == "__main__":
 
+    print('This program allows the user to input n (for n x n matrix), a, b, and c to solve\nPoisson\'s equation in a single dimension by turning it into a matrix\nusing Gaussian Jordan Elimination, Specialized Tridiagonal, and LU Decomposition\nBenchmarks and error calculations are also performed.\n\n')
+
     # get user input
     n_i = int(raw_input('Please input the size n of your desired matrix: '))
     a_i = int(raw_input('Please input the value of a for your desired matrix: '))
     b_i = int(raw_input('Please input the value of b for your desired matrix: '))
     c_i = int(raw_input('Please input the value of c for your desired matrix: '))
 
+    print('\nGeneral Algorithm')
     b_x, b_v = part_b(n_i, a_i, b_i, c_i)
     print(b_x)
     print(b_v)
 
+    print('\nSpecialized Tridiagonal Algorithm')
     c_x, c_v = part_c(n_i, a_i, b_i, c_i)
     print(c_x)
     print(c_v)
 
+    print('\nRelative Error')
     n, eps = part_d(n_i, a_i, b_i, c_i)
-    print(n)
-    print(eps)
+    print('n = ' + str(n))
+    print('error = ' + str(eps))
 
+    print('\nLinear Decomposition Algorithm')
     e_x, e_v = part_e(n_i, a_i, b_i, c_i)
     print(e_x)
     print(e_v)
 
     general_time, tridiag_time, LU_time = benchmarking(n_i, a_i, b_i, c_i)
+    print('\nGeneral Algorithm Time to Execute')
     print(general_time)
+    print('\nSpecialized Tridiagonal Algorithm Time to Execute')
     print(tridiag_time)
+    print('\nLU Decomposition Time to Execute')
     print(LU_time)
+    print('\n\n')
